0a943f0b291411e35e7850b8d492d9243606cc82
bryanJesus2003 <jesusbryan155@gmail.com>
2023-04-24 14:30:16 -0600

    v2

--- gatway-service/src/main/java/com/cloud/gatwayservice/GatwayServiceApplication.java
+++ gatway-service/src/main/java/com/cloud/gatwayservice/GatwayServiceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GatwayServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GatwayServiceApplication.class, args);
 	}
-
 }
