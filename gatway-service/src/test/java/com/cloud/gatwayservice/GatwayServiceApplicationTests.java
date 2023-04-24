270c8199b7324463f8088c47f5ff64c770b3c310
bryanJesus2003 <jesusbryan155@gmail.com>
2023-04-24 14:09:19 -0600

    v1

---
+++ gatway-service/src/test/java/com/cloud/gatwayservice/GatwayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.gatwayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatwayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
